9b62b0e30a7cae95f94e9864e81c4025ca761e75
fisehara <harald@balena.io>
2023-01-02 15:58:38 +0100

    Return $metadata resource as odata + openapi spec
    Returning odata and openapi specs in json format.
    Specs are scoped to the request permissions.
    Different users (roles) will receive different metadata endpoints
    and resources.
    
    Change-type: minor
    Signed-off-by: fisehara <harald@balena.io>

--- src/sbvr-api/permissions.ts
+++ src/sbvr-api/permissions.ts
@@ -312,7 +312,7 @@ const namespaceRelationships = (
 	});
 };
 
-type PermissionLookup = Dictionary<true | string[]>;
+export type PermissionLookup = Dictionary<true | string[]>;
 
 const getPermissionsLookup = env.createCache(
 	'permissionsLookup',
@@ -1619,7 +1619,7 @@ const getGuestPermissions = memoize(
 	{ promise: true },
 );
 
-const getReqPermissions = async (
+export const getReqPermissions = async (
 	req: PermissionReq,
 	odataBinds: ODataBinds = [],
 ) => {
